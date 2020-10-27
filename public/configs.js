1fca39f57d0f669cfd36c52559fd2860f08d54c0
胡振华 <impking@outlook.com>
2020-10-27 16:01:38 +0800

    配置分离

--- public/configs.js
+++ public/configs.js
@@ -1,12 +1,10 @@
-
 const urls = {
-    uri: 'http://118.31.184.21:9009', // 系统微服务
-    uri_company: 'http://118.31.184.21:9009', // 企业微服务
-    uri_monitor: 'http://118.31.184.21:9009', // 监控微服务
-    oss_uri: 'http://118.31.184.21:9009',
-    uri_login: 'http://118.31.184.21:5000',
-}
+  uri: 'http://118.31.184.21:9007', // 系统微服务
+  uri_company: 'http://118.31.184.21:9002', // 企业微服务
+  uri_monitor: 'http://118.31.184.21:9004', // 监控微服务
+  oss_uri: 'http://118.31.184.21:9006',
+};
 
-module.exports = {
-    urls,
-}
+window.__configs = {
+  urls,
+};
---
+++ src/utils/configsReader.ts
@@ -0,0 +1,46 @@
+import Store from './store';
+import configs from '@/common/configs';
+
+export function getUrls(): any {
+  return getConfigs().urls;
+}
+
+export function getConfigs(): any {
+  // 开发时读取src/common下的配置,部署时读取public目录下的配置.
+  if ('development' === process.env.NODE_ENV) {
+    return configs;
+  } else {
+    const _window: any = window;
+    const configs = _window.__configs;
+    return configs;
+  }
+}
+
+/**
+ * 从public/configs.js配置文件中读取所有urls
+ */
+export function getUrlByName(urlName: string): string {
+  const urls = getUrls();
+  const url = urls[urlName] as string;
+  if (url) {
+    return url;
+  } else {
+    throw new Error(`从配置文件中读取指定的url:${urlName} 失败...`);
+  }
+}
+
+/**
+ * 从public/configs.js配置文件中读取指定名字的配置.只找一层.
+ * @deprecated 暂未使用.
+ */
+export function getConfigByName(configName: string) {
+  const _window: any = window;
+  const configs = _window.__configs;
+  // const configs = Store.get('__configs')
+  const config = configs[configName];
+  if (config) {
+    return config;
+  } else {
+    throw new Error(`从配置文件中读取指定配置:${configName} 失败...`);
+  }
+}
