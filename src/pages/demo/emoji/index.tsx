93a60fc0f1543ef6114d962ad2eaf39fc00e528e
胡振华 <impking@outlook.com>
2020-12-18 11:28:08 +0800

    使用emoji表情

---
+++ src/pages/demo/emoji/index.tsx
@@ -0,0 +1,8 @@
+{/* <Column
+    cssClass={'emoji-font'}
+    key={'STATUS'}
+    dataField={'STATUS'}
+    caption={'状态'}
+    dataType={'date'}
+    cellRender={this.tableStatusColumnTemplate}
+/>, */}
